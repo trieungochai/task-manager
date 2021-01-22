2069b4a361f8eb6adcf1ab31bce6c3cd5478f843
Macbook <trieungochai.dev@gmail.com>
2021-01-22 17:43:35 +0700

    The Object ID

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,12 @@
 // CRUD
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -13,8 +16,9 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   const db = client.db(databaseName);
 
   // db.collection('users').insertOne({
-  //   name: 'Trieungochai',
-  //   age: 28
+  //   _id: id,
+  //   name: 'Cai',
+  //   age: 29
   // }, (error, result) => {
   //   if (error) {
   //     console.log('Unable to insert user!');
@@ -39,22 +43,22 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   //   console.log(result.ops);
   // });
 
-  db.collection('tasks').insertMany([
-    {
-      description: 'Clean the house',
-      completed: true
-    }, {
-      description: 'Renew inspection',
-      completed: false
-    }, {
-      description: 'Pot plants',
-      completed: false
-    }
-  ], (error, result) => {
-    if (error) {
-      console.log('Unable to insert tasks!');
-    }
+  // db.collection('tasks').insertMany([
+  //   {
+  //     description: 'Clean the house',
+  //     completed: true
+  //   }, {
+  //     description: 'Renew inspection',
+  //     completed: false
+  //   }, {
+  //     description: 'Pot plants',
+  //     completed: false
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     console.log('Unable to insert tasks!');
+  //   }
 
-    console.log(result.ops);
-  });
+  //   console.log(result.ops);
+  // });
 });
