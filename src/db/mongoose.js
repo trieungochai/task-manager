e2cb8113a2e4745fb3d73dc0b656a19aa8a324c1
Macbook <trieungochai.dev@gmail.com>
2021-01-27 01:37:02 +0700

    Resource Creation Endpoints (Part II)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,19 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
-});
-
-const Task = mongoose.model('task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
 });
