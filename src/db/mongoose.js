3c264dbf371e3095d099fb83afb7dc0bace92815
Macbook <trieungochai.dev@gmail.com>
2021-01-26 13:32:26 +0700

    Data Validation & Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,16 +8,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"!');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number!')
+      }
+    }
   }
 });
 
 // const me = new User({
-//   name: 'Cai',
-//   age: 'lol'
+//   name: 'Trieu Ngoc Hai',
+//   email: 'trieungochai.dev@gmail.com  ',
+//   password: 'Galaticos25121991'
 // });
 
 // me.save().then(() => {
@@ -25,19 +57,20 @@ const User = mongoose.model('User', {
 //   console.log('Error!', error);
 // });
 
-// Goal: Create a model for tasks
 const Task = mongoose.model('task', {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
 const task = new Task({
-  description: 'Learn the Mongoose Library',
-  completed: false
+  description: 'Blah Blah    ',
 });
 
 task.save().then(() => {
