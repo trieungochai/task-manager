4bcd7a7876228ecf0de88610fc57854f1d6a836c
Macbook <trieungochai.dev@gmail.com>
2021-02-02 11:48:00 +0700

    Converting Promise-Based Chain to Asyns/Await

--- play-ground/promise-chaining-2.js
+++ play-ground/promise-chaining-2.js
@@ -4,13 +4,33 @@
 // 3. Get & Print the total number of incomplete tasks
 
 require('../src/db/mongoose');
+const { count } = require('../src/models/task');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('600fab4ec668d3137f8ca507').then(task => {
-  console.log(task);
-  return Task.countDocuments({ completed: false });
-}).then(result => {
-  console.log(result);
+// Task.findByIdAndDelete('600fab4ec668d3137f8ca507').then(task => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false });
+// }).then(result => {
+//   console.log(result);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// Goal: Use async/await
+// 1. Create deleteTaskAndCount as an async function
+//    - Accept id of task to remove
+// 2. Use await to delete task and count up incomplete tasks
+// 3. Return the count
+// 4. Call the function and attach then/catch to lof results
+
+const deleteTaskAndCount = async(id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount('600fb746691902145bba34ec').then(count => {
+  console.log(count);
 }).catch(err => {
   console.log(err);
 });
--- play-ground/promise-chaining.js
+++ play-ground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('600fb3fea2f16213fcb5827f', { age: 1 }).then(user => {
-  console.log(user);
-  return User.countDocuments({ age: 1 });
-}).then(result => {
-  console.log(result);
+// User.findByIdAndUpdate('600fb3fea2f16213fcb5827f', { age: 1 }).then(user => {
+//   console.log(user);
+//   return User.countDocuments({ age: 1 });
+// }).then(result => {
+//   console.log(result);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+const updatePasswordAndCount = async(id, age) => {
+  const user = await User.findByIdAndUpdate(id, {age});
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updatePasswordAndCount('600fb3fea2f16213fcb5827f', 28).then(count => {
+  console.log(count);
 }).catch(err => {
   console.log(err);
 });
