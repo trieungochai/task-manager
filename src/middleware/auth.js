841ba071a66ff73844a446491a8725d7809a666f
Macbook <trieungochai.dev@gmail.com>
2021-02-07 19:09:35 +0700

    Accepting Authentication Tokens

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+// Middleware is going to store a new file for each piece of middleware that we're trying to define it
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const auth = async(req, res, next) => {
+  try {
+      const token = req.header('Authorization').replace('Bearer', '');
+      const decode = jwt.verify(token, 'thisismynewcourse');
+      const user = await User.findOne({ _id: decode._id, 'tokens.token': token });
+
+      if(!user) {
+        throw new Error();
+      }
+
+      req.user = user;
+      next();
+  } catch(err) {
+      res.status(401).send({ error: 'Please authenticate.' })
+  };
+};
+
+module.exports = auth;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 // Goal: Have signup send back auth token
@@ -27,13 +28,8 @@ router.post('/users/login', async(req, res) => {
   }
 });
 
-router.get('/users', async(req, res) => {
-  try {
-      const users = await User.find({});
-      res.send(users);
-  } catch(err) {
-      res.status(500).send(err);
-  };
+router.get('/users/me', auth, async(req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async(req, res) => {
