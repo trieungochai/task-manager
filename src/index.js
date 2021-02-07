3a6cd619d24a443cdc2f9beca2472823c6618341
Macbook <trieungochai.dev@gmail.com>
2021-02-07 18:31:26 +0700

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+// without middleware:  new req -> run router handler
+// with middleware:     new req -> do something -> run router handler
+// we can customize the behavior of the server to fit our needs.
+
+// Goal: Setup middleware for maintenance mode
+// 1. Register a new middleware function
+// 2. Send back a maintenance message with a 503 status code
+app.use((req, res, next) => {
+  res.status(503).send('Site is currently down. Check back soon!')
+});
+
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
