436c1768d792671eb70142cbdd790cc24e1c6f6a
Macbook <trieungochai.dev@gmail.com>
2021-02-18 11:08:47 +0700

    Serving up Files

--- src/routers/user.js
+++ src/routers/user.js
@@ -136,4 +136,19 @@ router.delete('/user/me/avatar', auth, async(req, res) => {
   res.send();
 });
 
+router.get('/users/:id/avatar', async(req, res) => {
+  try{
+    const user = await User.findById(req.params.id);
+
+    if(!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-Type', 'image/jpg');
+    res.send(user.avatar);
+  } catch(err) {
+    res.send(404).send();
+  }
+});
+
 module.exports = router;
